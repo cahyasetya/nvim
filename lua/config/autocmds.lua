046382333f7d39cd78d303d473060e31afd6377b
Cahya Setya Adhi <cahya.setyaa@gmail.com>
2024-09-20 12:47:16 +0700

    add auto save config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Autosave configuration
+vim.opt.updatetime = 1000  -- Set updatetime to 1000ms (1 second)
+
+-- Autosave when text changes or losing focus
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "FocusLost" }, {
+	pattern = "*",
+	callback = function()
+		if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" then
+			vim.api.nvim_command('silent! write')
+		end
+	end
+})
